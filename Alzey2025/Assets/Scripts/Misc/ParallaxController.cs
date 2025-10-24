9f18de585ba5ec154703a43f2f50fb0fe9eeaf60
Stefan Treffeisen <40838599+wolverwin@users.noreply.github.com>
2025-10-24 15:17:35 +0200

    Tag 5: Abschließende Änderungen

--- Alzey2025/Assets/Scripts/Misc/ParallaxController.cs
+++ Alzey2025/Assets/Scripts/Misc/ParallaxController.cs
@@ -9,29 +9,32 @@ namespace Misc {
         private float modifier;
 
         private Camera cam;
-        private float startPosition;
+        private float startPositionX;
+        private float startPositionY;
         private float width;
 
         private void Start() {
             width = GetComponent<SpriteRenderer>().bounds.size.x;
             cam = Camera.main;
-            startPosition = transform.position.x;
+            startPositionX = transform.position.x;
+            startPositionY = transform.position.y;
         }
 
         private void FixedUpdate() {
             // Calculate distance background move base on cam movement
-            float distance = cam.transform.position.x * modifier;
+            float distanceX = cam.transform.position.x * modifier;
+            float distanceY = cam.transform.position.y * modifier;
             float movement = cam.transform.position.x * (1 - modifier);
 
-            transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
+            transform.position = new Vector3(startPositionX + distanceX, startPositionY + distanceY, transform.position.z);
 
             // If background has reached the end of its length, adjust position for infinite scrolling
-            if (movement > startPosition + width) {
-                startPosition += width;
+            if (movement > startPositionX + width) {
+                startPositionX += width;
             }
 
-            if (movement < startPosition - width) {
-                startPosition -= width;
+            if (movement < startPositionX - width) {
+                startPositionX -= width;
             }
         }
     }
